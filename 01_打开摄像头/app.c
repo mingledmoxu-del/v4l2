b3e41a3aa822e96b8239365c2f0e1d12d3654ea5
omo <garrenchen@163.com>
2026-04-14 11:10:13 +0800

    摄像头格式

--- 01_打开摄像头/app.c
+++ 01_打开摄像头/app.c
@@ -1,6 +1,9 @@
 #include <fcntl.h>
 #include <linux/videodev2.h>
+#include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <sys/ioctl.h>
 #include <unistd.h>
 
@@ -14,10 +17,18 @@ int main() {
   struct v4l2_capability video_cap;
   struct v4l2_format video_fmt;
 
+  memset(&video_fmt, 0, sizeof(video_fmt));
+  video_fmt.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+
   ioctl(video_fd, VIDIOC_QUERYCAP, &video_cap);
 
   printf("video name is %s \n", video_cap.card);
+  ioctl(video_fd, VIDIOC_G_FMT, &video_fmt);
 
+  printf("当前分辨率是 %d * %d \n", video_fmt.fmt.pix.width,
+         video_fmt.fmt.pix.height);
+  char *p = (char *)&video_fmt.fmt.pix.pixelformat;
+  printf("当前像素格式: %c%c%c%c\n", p[0], p[1], p[2], p[3]);
   close(video_fd);
 
   printf("hello world\n");
